758e20270c8bdaa5b809452a18e70a77b005d2b6
WilliamPerezBeltran <williampbeltranprogramador@gmail.com>
2020-05-04 15:41:02 -0500

    Add style to components

--- src/js/components/RedirectUrl.js
+++ src/js/components/RedirectUrl.js
@@ -3,12 +3,45 @@ import "../styles/RedirectUrl.css";
 import Header from "./Header";
 
 class RedirectUrl extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = { value: "" };
+
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
+	handleChange(event) {
+		this.setState({ value: event.target.value });
+	}
+
+	handleSubmit(event) {
+		alert("A name was submitted: " + this.state.value);
+		event.preventDefault();
+	}
+
 	render() {
 		return (
 			<div>
-				<div>
-					<Header />
-					<h1>RedirectUrl</h1>
+				<Header />
+				<div className="container-short">
+					<div className="container-title">
+						<h2>Redireccionar url </h2>
+					</div>
+					<div>
+						<form
+							className="container-form"
+							onSubmit={this.handleSubmit}
+						>
+							<input
+								type="text"
+								value={this.state.value}
+								onChange={this.handleChange}
+								placeholder="Agregue su link "
+							/>
+							<input type="submit" value="Submit" />
+						</form>
+					</div>
 				</div>
 			</div>
 		);
--- src/js/components/ShortCode.js
+++ src/js/components/ShortCode.js
@@ -3,12 +3,45 @@ import "../styles/ShortCode.css";
 import Header from "./Header";
 
 class ShortCode extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = { value: "" };
+
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
+	handleChange(event) {
+		this.setState({ value: event.target.value });
+	}
+
+	handleSubmit(event) {
+		alert("A name was submitted: " + this.state.value);
+		event.preventDefault();
+	}
+
 	render() {
 		return (
 			<div>
-				<div>
-					<Header />
-					<h1>ShortCode</h1>
+				<Header />
+				<div className="container-short">
+					<div className="container-title">
+						<h2>Crear url reducida</h2>
+					</div>
+					<div>
+						<form
+							className="container-form"
+							onSubmit={this.handleSubmit}
+						>
+							<input
+								type="text"
+								value={this.state.value}
+								onChange={this.handleChange}
+								placeholder="redusca su link"
+							/>
+							<input type="submit" value="Submit" />
+						</form>
+					</div>
 				</div>
 			</div>
 		);
