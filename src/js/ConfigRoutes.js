583d27f68b5a4fd5ab512ef446613b91e7bed807
WilliamPerezBeltran <williampbeltranprogramador@gmail.com>
2020-05-04 11:12:54 -0500

    Add components

---
+++ src/js/ConfigRoutes.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import Home from './components/Home'
+import Header from './components/Header'
+import RedirectShortCode from './components/RedirectShortCode'
+import ShortCode from './components/ShortCode'
+// import Home from './components/Home'
+
+class ConfigRoutes extends React.Component {
+  render() {
+    return (
+		<div>
+			<Header/>
+			<Home/>
+			<RedirectShortCode/>
+			<ShortCode/>
+		</div>
+    );
+  }
+}
+export default ConfigRoutes;
+
